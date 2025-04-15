7a1e8f44eee9bf25afebe1535a69c6904b422e8b
Guilherme Nogueira <nogueira.gfn@gmail.com>
2025-04-14 23:16:40 -0300

    Add node spot on EKS module

--- AWS-RESOURCES/modules/aws/eks/iam-cluster.tf
+++ AWS-RESOURCES/modules/aws/eks/iam-cluster.tf
@@ -32,14 +32,6 @@ data:
 CONFIGMAPAWSAUTH
 }
 
-#locals {
-#  workers_userdata = <<USERDATA
-##!/bin/bash
-#set -o xtrace
-#/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.eks.endpoint}' --b64-cluster-ca '${aws_eks_cluster.eks.certificate_authority.0.data}' '${var.cluster_name}'
-#USERDATA
-#}
-
 locals {
   workers_userdata = <<USERDATA
 MIME-Version: 1.0
